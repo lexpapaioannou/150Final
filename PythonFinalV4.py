89306a6fe05bee22e4c527098e2d32b3262f44e5
lexpapaioannou <papaioannou.alexander1@gmail.com>
2016-12-02 01:49:37 -0600

    Add files via upload

---
+++ PythonFinalV4.py
@@ -0,0 +1,87 @@
+import csv
+
+while True:
+    columns = int(input("Input the number of columns in this data (up to 2): "))
+    if (columns > 2) or (columns < 1):
+        print("Please enter a new number.")
+        continue
+    else:
+        break
+
+z = ()
+y = ()
+x = ()
+w = () 
+v = ()
+u = ()
+t = ()
+s = ()
+r = ()
+q = ()
+moreRows = ""
+bName = ()
+rows = [z, y, x, w, v, u, t, s, r, q]
+numRows =  -1
+
+a = columns
+
+
+while True:
+        if a != 0:
+            iName = (input("Input the name of the first or following element: "))
+            bName = bName + (iName, )
+            a -= 1
+            continue
+        else:
+            break
+        break
+    
+while True:
+    while True:
+        numRows += 1
+        i = columns
+        while True:
+            if numRows != 10:
+                if i != 0:
+                    a=int(input("Input the first or following element: "))
+                    rows[numRows] = rows[numRows] + (a,)
+                    i -= 1
+                    continue
+                else:
+                    print(bName)
+                    print(rows[numRows])
+                    break
+                break
+            else:
+                break
+        moreRows = input("Do you wish to add more rows? (Up to 10) y/n: ")
+        if moreRows == "n":
+            break
+        elif numRows == 10:
+            break
+        elif moreRows == "y":
+            print(9 - numRows, " rows remaining.")
+            continue
+        else:
+            break
+    break
+
+labels = open("graphLabels.txt", "w")
+
+labels.write("%s \n %s\n" % (" \"" + bName[0] + "\"","\"" + bName[1] + "\""))
+
+labels.close()
+
+with open('outputFile.csv', 'w') as csvfile:
+    oFile = csv.writer(csvfile, delimiter = ',')
+    oFile.writerow(bName)
+    oFile.writerow(rows[0])
+    oFile.writerow(rows[1])
+    oFile.writerow(rows[2])
+    oFile.writerow(rows[3])
+    oFile.writerow(rows[4])
+    oFile.writerow(rows[5])
+    oFile.writerow(rows[6])
+    oFile.writerow(rows[7])
+    oFile.writerow(rows[8])
+    oFile.writerow(rows[9])
--- PythonFinalp2.py
+++ PythonFinalp2.py
@@ -2,10 +2,12 @@
 #   be running on Python 3.4.4
 #   run IDLE in 3.4 in 34 Bit
 #   make sure either PIL or pillow
+#   you will have to install pillow everytime you log into a new computer at the IC
 
 import PIL
 from PIL import Image
 
-graph = Image.open("Rplot.jpeg")
+graph = Image.open("C:\\Users\\apapaioannou\\downloads\\Rplot.png")
 
 graph.show()
+
